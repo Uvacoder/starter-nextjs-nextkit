37501c0c4eccd553f8131d4191c8e67865961487
uvacoder <uva22940coder@gmail.com>
2023-02-16 21:02:11 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: ['xo', 'xo-typescript'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/comma-dangle': 'off',
+		'@typescript-eslint/triple-slash-reference': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+	},
+	ignorePatterns: ['dist'],
+};
